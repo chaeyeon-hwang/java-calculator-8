ef69a5976accd1f624fec6a1814d4ce5e3b0bf55
chaeyeon-hwang <chaeyeon9034@naver.com>
2025-10-19 23:59:12 +0900

    feat(input): 콘솔 입력 기능 및 공백 처리 로직 추가
    # Conflicts:
    #	src/main/java/calculator/InputView.java

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -1,4 +1,14 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String inputExpression() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        if (input == null || input.trim().isEmpty()) {
+            return "";
+        }
+        return input;
+    }
 }
