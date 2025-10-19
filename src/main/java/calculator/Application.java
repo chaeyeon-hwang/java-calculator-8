465b4136b8739eec75df7cdd00c8aa1545e5858e
chaeyeon-hwang <chaeyeon9034@naver.com>
2025-10-19 20:50:11 +0900

    fix(application): InputView 메서드명 수정 (read → readExpression)

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         try {
 
-            String input = InputView.read();
+            String input = InputView.readExpression();
 
             String[] tokens = Expression.split(input);
 
