a5f2e6e54523779726f30094f532c0889f5973b3
chaeyeon-hwang <chaeyeon9034@naver.com>
2025-10-20 00:23:49 +0900

    feat(expression): 문자열을 구분자 기준으로 파싱하는 기능 구현

--- src/main/java/calculator/Expression.java
+++ src/main/java/calculator/Expression.java
@@ -1,18 +1,42 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Expression {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    private final String expression;
 
-    public static String[] split(String input) {
-        if (input == null || input.isEmpty()) {
-            return new String[]{"0"};
+    public Expression(String expression) {
+        this.expression = expression;
+    }
+
+    public List<String> getTokens() {
+        if (expression == null || expression.isEmpty()) {
+            return List.of("0");
         }
 
-        if (input.startsWith("//")) {
-            String delimiter = String.valueOf(input.charAt(2));
-            String target = input.substring(5);
-            return target.split(delimiter);
+
+        Matcher matcher = Pattern.compile("//(.)\\\\n(.*)").matcher(expression);
+        if (matcher.matches()) {
+            String customDelimiter = matcher.group(1);
+            String numbers = matcher.group(2);
+            return split(numbers, customDelimiter);
         }
 
-        return input.split("[,:]");
+        return split(expression, DEFAULT_DELIMITERS);
+    }
+
+    private List<String> split(String text, String delimiter) {
+        String[] tokens = text.split(delimiter);
+        List<String> result = new ArrayList<>();
+        for (String token : tokens) {
+            if (!token.isEmpty()) {
+                result.add(token);
+            }
+        }
+        return result;
     }
 }
