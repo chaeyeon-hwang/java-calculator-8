7bfb9b4a2df84eb8783fa9d9934086d258f5749b
chaeyeon-hwang <chaeyeon9034@naver.com>
2025-10-20 00:51:39 +0900

    feat(application): 프로그램 전체 실행 흐름 구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,15 +3,12 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         try {
-
-            String input = InputView.readExpression();
-
-            String[] tokens = Expression.split(input);
-
-            int result = Operands.sum(tokens);
-
+            String input = InputView.inputExpression();
+            Validator.validate(input);
+            Expression expression = new Expression(input);
+            Operands operands = new Operands(expression.getTokens());
+            int result = operands.sum();
             OutputView.printResult(result);
-
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
